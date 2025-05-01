2e6f515897075e38c6ba6ba011de7c66be98f429
doannhu <82296094+doannhu@users.noreply.github.com>
2025-05-01 11:14:03 +1000

    update product assessment entity

--- internal/credit_enquiry/entity/product_assessment.go
+++ internal/credit_enquiry/entity/product_assessment.go
@@ -2,18 +2,36 @@ package entity
 
 import "time"
 
+// ProductType represents the type of product
+type ProductType string
+
+const (
+	ProductTypeOwner    ProductType = "OWNER"
+	ProductTypeInvester ProductType = "INVESTER"
+)
+
+// InitialStructureRepaymentType represents the type of repayment structure
+type InitialStructureRepaymentType string
+
+const (
+	RepaymentTypePrincipalAndInterest InitialStructureRepaymentType = "PRINCIPLE AND INTEREST"
+	RepaymentTypeInterestOnly         InitialStructureRepaymentType = "INTEREST ONLY"
+)
+
 // ProductAssessment represents the product assessment entity
 type ProductAssessment struct {
-	ProductAssessmentID        string
-	ServiceabilityAssessmentID string
-	CreditEnquiryID            string
-	CreditEnquiryVersion       string
-	ProductCode                string
-	ProductName                string
-	LoanAmount                 float64
-	LoanPurpose                string
-	InitialStructureTermMonth  int64
-	InitialStructureIndexRate  int64
-	CreatedAt                  time.Time
-	UpdatedAt                  time.Time
+	ProductAssessmentID           string
+	ServiceabilityAssessmentID    string
+	CreditEnquiryID               string
+	CreditEnquiryVersion          string
+	ProductCode                   string
+	ProductName                   string
+	ProductType                   ProductType
+	LoanAmount                    float64
+	LoanPurpose                   string
+	InitialStructureTermMonth     int64
+	InitialStructureIndexRate     int64
+	InitialStructureRepaymentType InitialStructureRepaymentType
+	CreatedAt                     time.Time
+	UpdatedAt                     time.Time
 }
